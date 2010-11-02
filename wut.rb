40ea06a42db52c5d75a166c9d4c5cf00a8c615c9
Marcin Manek <marcin@damodred.net>
2010-11-02 16:57:03 -0700

    Ok, now really committed the .rb controller.

--- wut.rb
+++ wut.rb
@@ -1,7 +1,9 @@
 require 'sinatra'
+require 'haml'
 
 get '/' do
 	"Yes may I help you?"
+	haml :widget
 end
 get '/:name' do
 	"Let's get some includes and databases up in here, wot wot. Oh yes and #{params[:name]}."
