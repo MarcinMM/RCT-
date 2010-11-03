3c7768768c287a8269a1c3c5ec6371fcd25415b1
Marcin Manek <marcin@damodred.net>
2010-11-02 17:11:45 -0700

    Added 'rubygems' require for older ruby. Removed unused output from controller.

--- wut.rb
+++ wut.rb
@@ -1,10 +1,11 @@
+require 'rubygems'
 require 'sinatra'
 require 'haml'
 
 get '/' do
-	"Yes may I help you?"
 	haml :widget
 end
+
 get '/:name' do
 	"Let's get some includes and databases up in here, wot wot. Oh yes and #{params[:name]}."
 end
